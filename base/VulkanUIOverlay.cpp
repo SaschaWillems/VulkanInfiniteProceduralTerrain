b3de75e2d48f3f175a850202713e677c8b147fad
Sascha Willems <webmaster@saschawillems.de>
2022-09-05 20:11:44 +0200

    Minor UI changes

--- base/VulkanUIOverlay.cpp
+++ base/VulkanUIOverlay.cpp
@@ -28,6 +28,8 @@ namespace vks
 		ImGui::CreateContext();
 		// Color scheme
 		ImGuiStyle& style = ImGui::GetStyle();
+		style.Colors[ImGuiCol_WindowBg] = ImVec4(0.005f, 0.005f, 0.005f, 0.94f);
+		style.Colors[ImGuiCol_Border] = ImVec4(0.02f, 0.02f, 0.02f, 0.94f);
 		style.Colors[ImGuiCol_TitleBg] = ImVec4(1.0f, 0.0f, 0.0f, 1.0f);
 		style.Colors[ImGuiCol_TitleBgActive] = ImVec4(1.0f, 0.0f, 0.0f, 1.0f);
 		style.Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(1.0f, 0.0f, 0.0f, 0.1f);
@@ -303,77 +305,6 @@ namespace vks
 		VK_CHECK_RESULT(vkCreateGraphicsPipelines(device->logicalDevice, pipelineCache, 1, &pipelineCreateInfo, nullptr, &pipeline));
 	}
 
-	/** Update vertex and index buffer containing the imGui elements when required */
-	// @todo: remove
-	bool UIOverlay::update()
-	{
-		ImDrawData* imDrawData = ImGui::GetDrawData();
-		bool updateCmdBuffers = false;
-
-		if (!imDrawData) { return false; };
-
-		// Note: Alignment is done inside buffer creation
-		VkDeviceSize vertexBufferSize = imDrawData->TotalVtxCount * sizeof(ImDrawVert);
-		VkDeviceSize indexBufferSize = imDrawData->TotalIdxCount * sizeof(ImDrawIdx);
-
-		// Update buffers only if vertex or index count has been changed compared to current buffer size
-		if ((vertexBufferSize == 0) || (indexBufferSize == 0)) {
-			return false;
-		}
-
-		// @todo: comment
-		// @todo: wait for fences? vkGetFenceStatus
-		//if ((vertexCount != imDrawData->TotalVtxCount) || (indexCount != imDrawData->TotalIdxCount)) {
-		//	vkQueueWaitIdle(queue);
-		//}
-
-		/*
-		// Vertex buffer
-		if ((vertexBuffer.buffer == VK_NULL_HANDLE) || (vertexCount != imDrawData->TotalVtxCount)) {
-			vertexBuffer.unmap();
-			vertexBuffer.destroy();
-			VK_CHECK_RESULT(device->createBuffer(VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT, &vertexBuffer, vertexBufferSize));
-			vertexCount = imDrawData->TotalVtxCount;
-			vertexBuffer.unmap();
-			vertexBuffer.map();
-			updateCmdBuffers = true;
-		}
-
-		// Index buffer
-		VkDeviceSize indexSize = imDrawData->TotalIdxCount * sizeof(ImDrawIdx);
-		if ((indexBuffer.buffer == VK_NULL_HANDLE) || (indexCount < imDrawData->TotalIdxCount)) {
-			indexBuffer.unmap();
-			indexBuffer.destroy();
-			VK_CHECK_RESULT(device->createBuffer(VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT, &indexBuffer, indexBufferSize));
-			indexCount = imDrawData->TotalIdxCount;
-			indexBuffer.map();
-			updateCmdBuffers = true;
-		}
-		*/
-
-		/*
-
-		// Upload data
-		ImDrawVert* vtxDst = (ImDrawVert*)vertexBuffer.mapped;
-		ImDrawIdx* idxDst = (ImDrawIdx*)indexBuffer.mapped;
-
-		for (int n = 0; n < imDrawData->CmdListsCount; n++) {
-			const ImDrawList* cmd_list = imDrawData->CmdLists[n];
-			memcpy(vtxDst, cmd_list->VtxBuffer.Data, cmd_list->VtxBuffer.Size * sizeof(ImDrawVert));
-			memcpy(idxDst, cmd_list->IdxBuffer.Data, cmd_list->IdxBuffer.Size * sizeof(ImDrawIdx));
-			vtxDst += cmd_list->VtxBuffer.Size;
-			idxDst += cmd_list->IdxBuffer.Size;
-		}
-
-		// Flush to make writes visible to GPU
-		vertexBuffer.flush();
-		indexBuffer.flush();
-
-		*/
-
-		return updateCmdBuffers;
-	}
-
 	void UIOverlay::draw(const VkCommandBuffer commandBuffer, uint32_t frameIndex)
 	{
 		// @todo: move to calling function
@@ -418,7 +349,7 @@ namespace vks
 				scissorRect.offset.y = std::max((int32_t)(pcmd->ClipRect.y), 0);
 				scissorRect.extent.width = (uint32_t)(pcmd->ClipRect.z - pcmd->ClipRect.x);
 				scissorRect.extent.height = (uint32_t)(pcmd->ClipRect.w - pcmd->ClipRect.y);
-				vkCmdSetScissor(commandBuffer, 0, 1, &scissorRect);
+				//vkCmdSetScissor(commandBuffer, 0, 1, &scissorRect);
 				vkCmdDrawIndexed(commandBuffer, pcmd->ElemCount, 1, indexOffset, vertexOffset, 0);
 				indexOffset += pcmd->ElemCount;
 			}
